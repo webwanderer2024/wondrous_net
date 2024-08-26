ed1ee4e3c9648b94d2087184b0b0027e13d66e97
webwanderer2024 <156943509+webwanderer2024@users.noreply.github.com>
2024-08-26 22:30:45 +0500

    Update Mr. Game & Watch.py

--- Mr. Game & Watch.py
+++ Mr. Game & Watch.py
@@ -4,7 +4,6 @@ def solve_clocks(clocks_data,n):
     """Function that extracts messages from clocks for puzzle "Mr. Game & Watch" from MUMS Puzzle Hunt 2011 competition:     
        English - https://wondrousnet.blogspot.com/2024/01/solution-to-puzzle-mr-game-watch.html 
        Russian - https://wondrousnet.blogspot.com/2024/01/mr-game-watch.html
-
        Input:
            clocks_data: list or tuple of tuples, each of which represents one corresponding clock; each tuple consists of four parts:
                1) integer from 0 to 11 - number of hours
@@ -15,8 +14,7 @@ def solve_clocks(clocks_data,n):
                   if False, then a little bit more complex formula is used: minutes = (2/11)*(30*hours + angle - 360).
            n - integer: number of exctracted messages.
        Output:
-           string of uppercase messages for corresponding time values separated by the new line symbol."""
-    
+           string of uppercase messages for corresponding time values separated by the new line symbol."""    
     all_clocks_values = []
     # we collecting time values for clocks from bigger to smoller
     for clock in clocks_data:
@@ -57,15 +55,13 @@ def solve_clocks(clocks_data,n):
         all_messages += (message + '\n')
     return all_messages
 
-
-#representation of clocks from puzzle
+#representation of the clocks from puzzle
 clocks_data = ((1,'am',69.47564,True),(0,'pm',29.35372,True),(4,'pm',-8.70545,True),(8,'am',-128.60642,True),
                (1,'am',-0.66474,True),(2,'am',-32.37728,True),(5,'am',-104.12833,True),(8,'pm',148.35595,False),
                (9,'am',-169.34332,True),(7,'pm',-160.02279,True),(5,'am',-98.75045,True),(3,'am',-82.73052,True),
                (0,'pm',71.59333,True),(3,'pm',-60.38542,True),(3,'am',20.48941,True),(11,'am',59.18092,False),
                (7,'pm',-137.11121,True),(1,'am',59.86097,True),(2,'pm',24.33518,True),(4,'am',-8.80010,True),
                (8,'pm',-134.47923,True),(8,'am',164.75629,False),(5,'am',-44.09744,True),(2,'pm',40.46877,True))
-
 
 if __name__ == '__main__':
     messages = solve_clocks(clocks_data,5) # command to extract all messages from clocks in puzzle
